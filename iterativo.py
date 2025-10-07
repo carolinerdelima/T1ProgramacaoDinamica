cc5e3df05bb7469597595ec0dc9d4bdd492db100
Caroline Lima <carolinelima013@hotmail.com>
2025-10-07 00:46:52 -0300

    Ajustando versão iterativa

--- iterativo.py
+++ iterativo.py
@@ -1,40 +1,39 @@
 # Usa programação dinâmica iterativa -> bottom-up.
 # Retorna uma tupla -> número mínimo de operações, lista de operações.
 
-def menor_numero_de_operacoes(n: int):
-    operacoes = [0] * (n + 1)
-    anterior = [0] * (n + 1)
-    op_usada = [""] * (n + 1)
+def menorNumeroDeOperacoes(num: int):
+    operacoes = [0] * (num + 1)
+    anterior = [0] * (num + 1)
+    opUsada = [""] * (num + 1)
 
     # Caso base
     operacoes[1] = 0
 
-    # Calcula de 2 até n
-    for i in range(2, n + 1):
+    for i in range(2, num + 1):
         # Decremento de 1
-        menor_custo = operacoes[i - 1] + 1
+        menorNumOp = operacoes[i - 1] + 1
         anterior[i] = i - 1
-        op_usada[i] = "-1"
+        opUsada[i] = "-1"
 
         # Divisão por 2 (se possível)
-        if i % 2 == 0 and operacoes[i // 2] + 1 < menor_custo:
-            menor_custo = operacoes[i // 2] + 1
+        if i % 2 == 0 and operacoes[i // 2] + 1 < menorNumOp:
+            menorNumOp = operacoes[i // 2] + 1
             anterior[i] = i // 2
-            op_usada[i] = "/2"
+            opUsada[i] = "/2"
 
         # Divisão por 3 (se possível)
-        if i % 3 == 0 and operacoes[i // 3] + 1 < menor_custo:
-            menor_custo = operacoes[i // 3] + 1
+        if i % 3 == 0 and operacoes[i // 3] + 1 < menorNumOp:
+            menorNumOp = operacoes[i // 3] + 1
             anterior[i] = i // 3
-            op_usada[i] = "/3"
+            opUsada[i] = "/3"
 
-        operacoes[i] = menor_custo
+        operacoes[i] = menorNumOp
 
-    # Reconstrói o caminho percorrido a partir de n até 1
+    # Reconstrói o caminho percorrido a partir de num até 1
     caminho = []
-    atual = n
+    atual = num
     while atual != 1:
-        caminho.append(op_usada[atual])
+        caminho.append(opUsada[atual])
         atual = anterior[atual]
 
-    return operacoes[n], caminho
+    return operacoes[num], caminho
